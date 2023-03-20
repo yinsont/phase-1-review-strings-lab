a27097fff023aa4fe0d7bf3231763aa145ff0ef3
Yinson Tso <yinson.tso@gmail.com>
2023-03-20 12:23:36 -0400

    strings review

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'food'
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase()
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`
